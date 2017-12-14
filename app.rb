c8864e17f7aabfbe00f995eb607d5046bd0aad67
Michael Evans <michael@michaelgregoryevans.com>
2017-12-14 20:05:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "dragon"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    #binding.pry
+    session[:item] = params[:item]
+    @session = session
+    #binding.pry
+    erb :show
+  end
 end
